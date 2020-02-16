fbb95cbe073f12eb7dc9f478e0c6246cfded6fb5
Andrew Bartish <bartishco@gmail.com>
2020-02-16 15:29:36 +0200

    Додано видалення файлів. Додано примітивний текстовий інтерфейс. Кількість g-коду зменшилось)

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 struct Word{
     char eng[20];
     char ua[20];
-};
+}word;
 const int LENGHT = 20;
 char PATH_UA[100] = "/home/andrew/CLionProjects/mod2_classWork1(dictionary)/Dictionary/UAeng/";
 char PATH_ENG[100] = "/home/andrew/CLionProjects/mod2_classWork1(dictionary)/Dictionary/ENGua/";
@@ -15,7 +15,6 @@ char PATH_ENG[100] = "/home/andrew/CLionProjects/mod2_classWork1(dictionary)/Dic
 
 
 void writeToUa(char ua[LENGHT], char eng[LENGHT]){
-    Word word;
     strcpy(word.ua, ua);
     strcpy(word.eng, eng);
 
@@ -27,9 +26,7 @@ void writeToUa(char ua[LENGHT], char eng[LENGHT]){
     stream.write((char*)&word, sizeof(Word));
     stream.close();
 }
-
 void writeToEng(char eng[LENGHT], char ua[LENGHT]){
-    Word word;
     strcpy(word.ua, ua);
     strcpy(word.eng, eng);
 
@@ -41,68 +38,137 @@ void writeToEng(char eng[LENGHT], char ua[LENGHT]){
     stream.write((char*)&word, sizeof(Word));
     stream.close();
 }
+void terminalWrite(){
+    const char* msg1 = "ENTER THE WORD:";
+    const char* msg2 = "ENTER THE TRANSLATION:";
+    char first[LENGHT];
+    char second[LENGHT];
 
-void readFromUa(char ua[LENGHT]){
-    Word word;
-    char myPath[100];
-    strcpy(myPath, PATH_UA);
-    strcat(strcat(myPath, ua), ".data");
+    cout << msg1 << endl;
+    cin.get();
+    cin.getline(first, LENGHT);
+    cout << msg2 << endl;
+    cin.getline(second, LENGHT);
 
-    ifstream stream(myPath, ios::binary | ios::in);
-
-    while(true){
-        stream.read((char*)&word, sizeof(Word));
-        if(stream.eof()) break;
-        cout << word.eng << endl;
+    if((int)first[0] < 0) {
+        writeToUa(first, second);
+        writeToEng(second, first);
+    }else {
+        writeToEng(first, second);
+        writeToUa(second, first);
     }
 }
 
-void readFromEng(char eng[LENGHT]){
-    Word word;
+bool readFromUa(char ua[LENGHT]){
     char myPath[100];
+    strcpy(myPath, PATH_UA);
+    strcat(strcat(myPath, ua), ".data");
+
+    ifstream stream(myPath, ios::binary | ios::in);
+
+    if(stream.good()){
+        while(true){
+            stream.read((char*)&word, sizeof(Word));
+            if(stream.eof()) break;
+            cout << word.eng << endl;
+        }
+    }else{
+        return false;
+    }
+    return true;
+}
+bool readFromEng(char eng[LENGHT]){
+    char myPath[100];
+
     strcpy(myPath, PATH_ENG);
     strcat(strcat(myPath, eng), ".data");
 
     ifstream stream(myPath, ios::binary | ios::in);
 
-    while(true){
-        stream.read((char*)&word, sizeof(Word));
-        if(stream.eof()) break;
-        cout << word.ua << endl;
+    if(stream.good()){
+        while(true){
+            stream.read((char*)&word, sizeof(Word));
+            if(stream.eof()) break;
+            cout << word.ua << endl;
+        }
+    }else{
+        return false;
+    }
+    return true;
+}
+void terminalRead(){
+    const char* msg = "ENTER THE WORD WHOSE TRANSLATIONS YOU WANT TO WATCH:";
+    char first[LENGHT];
+
+    cout << msg << endl;
+    cin.get();
+    cin.getline(first, LENGHT);
+
+    if ((int) first[0] > 0) {
+        cout << (readFromEng(first) ? "" : "Перекладу не знайдено") << endl;
+    } else {
+        cout << (readFromUa(first) ? "" : "Перекладу не знайдено") << endl;
+    }
+}
+
+bool removeUaTranslate(char ua[LENGHT]){
+    char myPath[100];
+    strcpy(myPath, PATH_UA);
+    strcat(strcat(myPath, ua), ".data");
+    return remove(myPath);
+}
+bool removeEngTranslate(char eng[LENGHT]){
+    char myPath[100];
+    strcpy(myPath, PATH_ENG);
+    strcat(strcat(myPath, eng), ".data");
+    return remove(myPath);
+}
+void terminalRemove(){
+    const char* msg = "ENTER THE WORD WHOSE TRANSLATIONS YOU WANT TO REMOVE:";
+    char first[LENGHT];
+
+    cout << msg << endl;
+    cin.get();
+    cin.getline(first, LENGHT);
+
+    if((int)first[0] > 0) {
+        if(!removeEngTranslate(first)) cout << "SUCCESS" << endl;
+        else cout << "ERROR!" << endl;
+    }else {
+        if(!removeUaTranslate(first)) cout << "SUCCESS" << endl;
+        else cout << "ERROR!" << endl;
     }
 }
 
 bool menu(){
+    const char* msg = "0 - exit\n"
+                      "1 - add translate\n"
+                      "2 - get translate\n"
+                      "3 - delete word\n"
+                      "4 - delete translate";
     int c;
-    char first[LENGHT];
-    char second[LENGHT];
+
+    cout << msg << endl;
     cin >> c;
 
-    if(c == 0){
-        return 0;
-    } else if(c == 1){
-        cin.get();
-        cin.getline(first, LENGHT);
-        cin.getline(second, LENGHT);
-        if((int)first[0] < 0) {
-            writeToUa(first, second);
-        }else {
-            writeToEng(first, second);
-        }
-
-    } else if(c == 2){
-        cin.getline(first, LENGHT);
-        if(first[0] > 125) readFromUa(first);
-        else readFromEng(first);
-    }else{
-        return 0;
+    switch (c){
+        case 1:
+            terminalWrite();
+            break;
+        case 2:
+            terminalRead();
+            break;
+        case 3:
+            break;
+        case 4:
+            terminalRemove();
+            break;
+        default:
+            return false;
     }
-
-    return 1;
+    return true;
 }
 
 int main() {
     while (menu()) {}
-
-
 }
